10ea9f6e171381f2527a1224753b948043994bc1
Brad Gibson <brad@tpfs.io>
2020-01-08 18:14:27 -0800

    learning exercise-converted all tests to property tests: green

--- rust/src/gilded_rose/unit_tests/backstage_pass.rs
+++ rust/src/gilded_rose/unit_tests/backstage_pass.rs
@@ -1,86 +1,88 @@
 use super::*;
 
-#[test]
-pub fn backstage_pass_quality_increases_by_1_as_it_ages_long_before_sell_in_date() {
-    // given
-    let expected_quality = 1;
-    let name = String::from("Backstage passes to a TAFKAL80ETC concert");
-    let sell_in = 11;
-    let quality = 0;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+proptest! {
+    #[test]
+    fn backstage_pass_quality_increases_by_1_as_it_ages_long_before_sell_in_date(
+            sell_in in 11..=i32::max_value(),
+            quality in 0..=49) {
+        // given
+        let expected_quality = quality + 1;
+        let name = String::from("Backstage passes to a TAFKAL80ETC concert");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
-}
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
 
-#[test]
-pub fn backstage_pass_quality_increases_by_three_near_sell_in_date() {
-    // given
-    let expected_quality = 3;
-    let name = String::from("Backstage passes to a TAFKAL80ETC concert");
-    let sell_in = 5;
-    let quality = 0;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+    #[test]
+    fn backstage_pass_quality_increases_by_three_near_sell_in_date(
+            sell_in in 1..=5,
+            quality in 0..=47) {
+        // given
+        let expected_quality = quality + 3;
+        let name = String::from("Backstage passes to a TAFKAL80ETC concert");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
-}
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
 
-#[test]
-pub fn backstage_pass_quality_increases_by_two_somewhat_near_sell_in_date() {
-    // given
-    let expected_quality = 2;
-    let name = String::from("Backstage passes to a TAFKAL80ETC concert");
-    let sell_in = 10;
-    let quality = 0;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+    #[test]
+    fn backstage_pass_quality_increases_by_two_somewhat_near_sell_in_date(
+            sell_in in 6..=10,
+            quality in 0..=48) {
+        // given
+        let expected_quality = quality + 2;
+        let name = String::from("Backstage passes to a TAFKAL80ETC concert");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
-}
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
 
-#[test]
-pub fn backstage_pass_quality_does_not_exceed_50_before_sell_in_date() {
-    // given
-    let expected_quality = 50;
-    let name = String::from("Backstage passes to a TAFKAL80ETC concert");
-    let sell_in = 5;
-    let quality = 50;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+    #[test]
+    fn backstage_pass_quality_does_not_exceed_50_before_sell_in_date(
+            sell_in in 1..=i32::max_value(),
+            quality in 49..=50) {
+        // given
+        let expected_quality = 50;
+        let name = String::from("Backstage passes to a TAFKAL80ETC concert");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
-}
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
 
-#[test]
-pub fn backstage_pass_quality_drops_to_0_after_sell_in_date() {
-    // given
-    let expected_quality = 0;
-    let name = String::from("Backstage passes to a TAFKAL80ETC concert");
-    let sell_in = -1;
-    let quality = 49;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+    #[test]
+    fn backstage_pass_quality_drops_to_0_at_and_after_sell_in_date(
+            sell_in in i32::min_value()..=0,
+            quality in 0..=50) {
+        // given
+        let expected_quality = 0;
+        let name = String::from("Backstage passes to a TAFKAL80ETC concert");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
 }
--- rust/src/gilded_rose/unit_tests/sulfuras.rs
+++ rust/src/gilded_rose/unit_tests/sulfuras.rs
@@ -1,54 +1,37 @@
 use super::*;
 
-#[test]
-pub fn sulfuras_sell_in_does_not_decrease_in_quality() {
-    // given
-    let expected_quality = 6;
-    let name = String::from("Sulfuras, Hand of Ragnaros");
-    let sell_in = 4;
-    let quality = expected_quality;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
+proptest! {
+    #[test]
+    fn sulfuras_sell_in_does_not_decrease_in_quality(
+            sell_in in i32::min_value()..=i32::max_value(),
+            quality in 0..=50) {
+        // given
+        let expected_quality = quality;
+        let name = String::from("Sulfuras, Hand of Ragnaros");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
 
-    // when
-    rose.update_quality();
+        // when
+        rose.update_quality();
 
-    // then
-    assert_eq!(rose.items[0].quality, expected_quality);
+        // then
+        assert_eq!(rose.items[0].quality, expected_quality);
+    }
+
+    #[test]
+    fn sulfuras_sell_in_does_not_age(
+            sell_in in i32::min_value()..=i32::max_value(),
+            quality in 0..=50) {
+        // given
+        let expected_sell_in = sell_in;
+        let name = String::from("Sulfuras, Hand of Ragnaros");
+        let items = vec![Item::new(name, sell_in, quality)];
+        let mut rose = GildedRose::new(items);
+
+        // when
+        rose.update_quality();
+
+        // then
+        assert_eq!(rose.items[0].sell_in, expected_sell_in);
+    }
 }
-
-#[test]
-pub fn sulfuras_sell_in_does_not_age_before_sell_in_date() {
-    // given
-    let expected_sell_in = 6;
-    let name = String::from("Sulfuras, Hand of Ragnaros");
-    let sell_in = expected_sell_in;
-    let quality = 49;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
-
-    // when
-    rose.update_quality();
-
-    // then
-    assert_eq!(rose.items[0].sell_in, expected_sell_in);
-}
-
-
-#[test]
-pub fn sulfuras_sell_in_does_not_age_after_sell_in_date() {
-    // given
-    let expected_sell_in = -3;
-    let name = String::from("Sulfuras, Hand of Ragnaros");
-    let sell_in = expected_sell_in;
-    let quality = 49;
-    let items = vec![Item::new(name, sell_in, quality)];
-    let mut rose = GildedRose::new(items);
-
-    // when
-    rose.update_quality();
-
-    // then
-    assert_eq!(rose.items[0].sell_in, expected_sell_in);
-}
-
