4aa14f921dd3b4632ac221da23d39b541b3b2903
u007d <b2b@humanenginuity.com>
2020-01-25 07:42:11 -0800

    separated all item aging logic: green

--- rust/src/gilded_rose.rs
+++ rust/src/gilded_rose.rs
@@ -28,38 +28,49 @@ impl GildedRose {
     #[allow(clippy::integer_arithmetic)]
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-            {
-                if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" {
-                    item.quality -= 1;
-                }
-            } else if item.quality < 50 {
-                item.quality += 1;
-
-                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                    if item.sell_in < 11 && item.quality < 50 {
+            match item.name.as_str() {
+                "Aged Brie" => {
+                    if item.quality < 50 {
                         item.quality += 1;
                     }
 
-                    if item.sell_in < 6 && item.quality < 50 {
+                    item.sell_in = item.sell_in.saturating_sub(1);
+
+                    if item.sell_in < 0 &&
+                        item.quality < 50 {
                         item.quality += 1;
                     }
                 }
-            }
+                "Backstage passes to a TAFKAL80ETC concert" => {
+                    if item.quality < 50 {
+                        item.quality += 1;
+                        if item.sell_in < 11 && item.quality < 50 {
+                            item.quality += 1;
+                        }
+                        if item.sell_in < 6 && item.quality < 50 {
+                            item.quality += 1;
+                        }
+                    }
 
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in.saturating_sub(1);
-            }
+                    item.sell_in = item.sell_in.saturating_sub(1);
 
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.sell_in < 0 {
                         item.quality = 0;
-                    } else if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" {
+                    }
+                }
+                "Sulfuras, Hand of Ragnaros" => {}
+                _ => {
+                    if item.quality > 0 {
                         item.quality -= 1;
                     }
-                } else if item.quality < 50 {
-                    item.quality += 1;
+
+
+                    item.sell_in = item.sell_in.saturating_sub(1);
+
+                    if item.sell_in < 0 &&
+                        item.quality > 0 {
+                        item.quality -= 1;
+                    }
                 }
             }
         }
